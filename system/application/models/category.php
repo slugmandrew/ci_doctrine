e746a2d8b052bf81fe166c93c10b378b7af5888e
Drew Spencer <slugmandrew@gmail.com>
2009-11-15 19:14:22 +0000

    Initial Commit

---
+++ system/application/models/category.php
@@ -0,0 +1,18 @@
+<?php
+class Category extends Doctrine_Record
+{
+
+	public function setTableDefinition()
+	{
+		$this->hasColumn('title', 'string', 255);
+	}
+
+	public function setUp()
+	{
+		$this->hasMany('Forum as Forums', array(
+			'local' => 'id',
+			'foreign' => 'category_id'
+		));
+	}
+}
+?>
